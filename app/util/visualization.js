22412d3339efb00c9ad566b44197a8b6c9511ab9
Ryan Keller <rykeller@uw.edu>
2018-04-21 16:42:34 -0700

    Weird issues w/ color legends?

--- app/util/visualization.js
+++ app/util/visualization.js
@@ -2,50 +2,24 @@
 COLORS
 */
 export const statusColors = {
-  'Withdrawn': '#d3d3d3',
-  'Draft': '#BBDEFB',
-  'Submitted': '#8eacbb',
-  'In Review': '#6ec6ff',
-  'Awaiting Decision': '#2196f3',
-  'Partially Funded': '#80e27e',
-  'Funded': '#4caf50',
-  'Denied': '#ff7961'
+  Free: '#4caf50',
+  Reserved: '#6ec6ff',
+  Occupied: '#ff7961'
 }
-export const quarterColors = {
-  'Autumn': '#bf360c',
-  'Winter': '#01579b',
-  'Spring': '#1b5e20',
-  'Blocks / Special Projects': '#4b2e83',
-  'Remaining Funding': '#d3d3d3'
-}
-export const brandColors = {
-  'Purple': '#4b2e83',
-  'Gold': '#b7a57a',
-  'Metallic Gold': '#85754d',
-  'Light Gray': '#d9d9d9',
-  'Dark Gray': '#444444'
+const locationColors = {
+  Factoria: '#e20074',  //  '#ff009f',
+  Bothell: '#5a5a5a',
+  Snoqualmie: '#999B9E'
 }
 export const DIVERGING_COLOR_SCALE = ['#00939C', '#85C4C8', '#EC9370', '#C22E00']
 
 /*
 LEGENDS
 */
-export const statusLegend = [
-  { title: 'Withdrawn', color: statusColors['Withdrawn'] },
-  { title: 'Draft', color: statusColors['Draft'] },
-  { title: 'Submitted', color: statusColors['Submitted'] },
-  { title: 'In Review', color: statusColors['In Review'] },
-  { title: 'Awaiting Decision', color: statusColors['Awaiting Decision'] },
-  { title: 'Partially Funded', color: statusColors['Partially Funded'] },
-  { title: 'Funded', color: statusColors['Funded'] },
-  { title: 'Denied', color: statusColors['Denied'] }
-]
-export const quarterlyFundingLegend = [
-  { title: 'Autumn', color: quarterColors['Autumn'] },
-  { title: 'Winter', color: quarterColors['Winter'] },
-  { title: 'Spring', color: quarterColors['Spring'] },
-  { title: 'Blocks / Special Projects', color: brandColors['Purple'] },
-  { title: 'Remaining Funding', color: brandColors['Light Gray'] }
+export const locationLegend = [
+  { title: 'Factoria', color: locationColors.Factoria },
+  { title: 'Bothell', color: locationColors.Bothell },
+  { title: 'Snoqualmie', color: locationColors.Snoqualmie }
 ]
 
 /*
@@ -53,9 +27,7 @@ Top Level Export
 */
 export default {
   statusColors,
-  quarterColors,
-  brandColors,
+  locationColors,
   DIVERGING_COLOR_SCALE,
-  statusLegend,
-  quarterlyFundingLegend
+  locationLegend
 }
--- app/views/Dashboard/Dashboard.js
+++ app/views/Dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import { connect } from 'react-redux'
 import { connectRequest } from 'redux-query'
 
 import { Sunburst, LabelSeries, Hint, DiscreteColorLegend } from 'react-vis'
-import { statusColors, statusLegend, DIVERGING_COLOR_SCALE } from '../../util/visualization'
+import { locationColors, statusColors, locationLegend } from '../../util/visualization'
 
 import api from '../../services'
 
@@ -27,21 +27,84 @@ class Dashboard extends React.Component {
   static defaultProps = {
     buildings: []
   }
-  render ({ buildings } = this.props) {
+  jss = {
+    labels: {
+      primary: { fill: '#5a5a5a', fontSize: '46px', textAnchor: 'middle' },
+      secondary: { fill: '#000', fontSize: '16px', textAnchor: 'middle' }
+    }
+  }
+  state = {
+    hoveredCell: {}
+  }
+
+  // D3 utility - pulls the angle of a hovered cell, used to generate D3's styles
+  buildValue (hoveredCell) {
+    const { radius, angle, angle0 } = hoveredCell
+    const truedAngle = (angle + angle0) / 2
+    return {
+      x: radius * Math.cos(truedAngle),
+      y: radius * Math.sin(truedAngle)
+    }
+  }
+  //  D3 events for selecting cells to pull data from for hints
+  onValueMouseOver = (v) => this.setState({ hoveredCell: v.x && v.y ? v : false })
+  onValueMouseOut= (v) => this.setState({ hoveredCell: false })
+
+  // Per D3 schema, size is only kept in child leaves
+  // getSize iterates through a parent and counts its
+  // size via closure and a recursive function
+  getSizeOfParent = (parent) => {
+    if (parent.size) {
+      return parent.size
+    } else {
+      let count = 0
+      // Recursively count child nodes, the size prop is the target val
+      const countLeaves = (node) => {
+        if (node.children) {
+          for (let child of node.children) {
+            // Has children
+            if (child.children) {
+              countLeaves(child)
+            // Has leaves
+            } else if (child.size) {
+              count += child.size
+            }
+          }
+        }
+      }
+      countLeaves(parent)
+      return count
+    }
+  }
+
+  render (
+    { jss } = this,
+    { buildings } = this.props,
+    { hoveredCell } = this.state
+  ) {
     let data = []
     for (let building of buildings) {
-      const { name, rooms } = building
+      const { name, location, rooms } = building
       const used = rooms.filter(r => r.occupied === true).length
       const unused = rooms.length - used
       data.push({
-        title: name,
+        title: `rooms in ${name}`,
+        // color: (locationColors[location] || locationColors['Factoria']),
+        // color: locationColors['Factoria'],
+        color: '#e20074',
         children: [
-          { title: 'Used', size: used, color: DIVERGING_COLOR_SCALE[0] },
-          { title: 'Unused', size: unused, color: DIVERGING_COLOR_SCALE[1] }
+          { title: 'Occupied', size: used, color: statusColors['Occupied'] },
+          // TODO: 'Reserved' - rooms reserved but not checked in
+          { title: 'Reserved', size: 0 },
+          { title: 'Free', size: unused, color: statusColors['Free'] }
         ]
       })
     }
-    console.log(data)
+    const labels = [
+      { x: 0, y: -5, label: (data.length || 0), style: jss.labels.primary },
+      { x: 0, y: -20, label: 'Rooms Available', style: jss.labels.secondary }
+    ]
+    console.log(data, locationColors)
     return (
       <article>
         <Helmet title='Dashboard' />
@@ -52,16 +115,30 @@ class Dashboard extends React.Component {
         </section>
         <section>
           <Sunburst
-            animation={{damping: 20, stiffness: 300}}
-            // data={data}
-            data={{ title: 'Title', children: data }}
-            // colorType='category'
-            hideRootNode
+            className='rv-inline'
+            data={{ title: 'Buildings', color: '#FFF', children: data }}
+            // hideRootNode
             colorType='literal'
-            // colorRange={DIVERGING_COLOR_SCALE}
             style={{ stroke: '#fff' }}
             height={300}
             width={350}
+            onValueMouseOver={this.onValueMouseOver}
+            onValueMouseOut={this.onValueMouseOut}
+          >
+            <LabelSeries data={labels} />
+            {hoveredCell && hoveredCell.title
+              // Generates tooltips onMouseOver w/ dynamic JSS styles
+              ? <Hint value={this.buildValue(hoveredCell)}>
+                <div className='rv-tooltip'>
+                  <div className='rv-box' style={{ background: hoveredCell.color }} />
+                  {`${this.getSizeOfParent(hoveredCell)} ${hoveredCell.title}`}
+                </div>
+              </Hint>
+            : null}
+          </Sunburst>
+          <DiscreteColorLegend
+            className='rv-inline'
+            items={locationLegend}
           />
         </section>
       </article>
